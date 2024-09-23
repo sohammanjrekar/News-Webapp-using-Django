319f8574099f4ad5ced7202e56e40aef08b969a8
soham shrikant manjrekar <mrsohammanjrekar@gmail.com>
2024-09-23 08:16:51 +0530

    upload

--- fastnews/fastnews/settings.py
+++ fastnews/fastnews/settings.py
@@ -1,15 +1,12 @@
-
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8h#52*&2lcr$g=$^8fjj11+#-u!s51xu19s8a*h7ng-uopvzp2"
-
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.craftlyweb.com']
-
 # Application definition
-
 INSTALLED_APPS = [
+    # 'whitenoise.runserver_nonstatic',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -18,9 +15,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'backend'
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",l
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -28,9 +25,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 ROOT_URLCONF = "fastnews.urls"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -46,21 +41,14 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = "fastnews.wsgi.application"
-
-
 # Database
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -75,22 +63,19 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+# STATIC_DIRS=[os.path.join(BASE_DIR,'backend/static')]
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+# pip install whitenoise
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
